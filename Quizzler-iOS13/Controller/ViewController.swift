7af58692d972be7e1141fa9b52e3ca7a2e92651c
Jantsen <deroest6@hotmail.com>
2021-10-20 20:12:05 -0600

    Refactor to the MVC standard - Theres still a lingering error with "Question" in QuizBrain struct

---
+++ Quizzler-iOS13/Controller/ViewController.swift
@@ -0,0 +1,63 @@
+//
+//  ViewController.swift
+//  Quizzler-iOS13
+//
+//  Created by Angela Yu on 12/07/2019.
+//  Copyright © 2019 The App Brewery. All rights reserved.
+//
+
+import UIKit
+import Foundation
+
+class ViewController: UIViewController {
+    
+    @IBOutlet weak var questionLabel: UILabel!
+    @IBOutlet weak var progressBar: UIProgressView!
+    @IBOutlet weak var trueButton: UIButton!
+    @IBOutlet weak var falseButton: UIButton!
+
+    func applyCornerRadius () {
+        @IBInspectable var cornerRadius: CGFloat {
+            set {
+                layer.cornerRadius = newValue
+            }
+            get {
+                return layer.cornerRadius
+            }
+        }
+    }
+    
+    var quizBrain = QuizBrain()
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+        updateUI()
+    }
+    
+    @IBAction func answerButtonPressed(_ sender: UIButton) {
+        
+        let userAnswer = sender.currentTitle //True, False
+        let userGotItRight = quizBrain.checkAnswer(userAnswer!)
+        
+        if userGotItRight {
+            sender.backgroundColor = UIColor.green
+        } else {
+            sender.backgroundColor = UIColor.red
+        }
+        
+        quizBrain.nextQuestion()
+        
+        DispatchQueue.main.asyncAfter(deadline: .now() + 0.25) {
+            self.updateUI()
+        }
+    }
+    
+    func updateUI() {
+        questionLabel.text = quizBrain.getQuestionText()
+        progressBar.progress = quizBrain.getProgress()
+        trueButton.backgroundColor = UIColor.clear
+        falseButton.backgroundColor = UIColor.clear
+    }
+}
+
